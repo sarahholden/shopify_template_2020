e36c3af3373e5229c81994dc1b44965c7e95e9a5
sarahholden <sarahbeth.holden@gmail.com>
2020-06-12 21:22:28 -0700

    Updates to PP Round 1

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -87,59 +87,72 @@ $(document).ready(function() {
   /* ---------------------------------------------
     NAV FIXED WHEN SCROLLING UP
   ------------------------------------------------ */
-  // if ($(window).scrollTop() < 30) {
-  //   $('.nav-wrapper').addClass('loaded');
-  // }
-  //
-  // $('.nav-wrapper').addClass('fade-in');
-  //
-  // // Hide Header on on scroll down
-  // var didScroll;
-  // var lastScrollTop = 0;
-  // var delta = 5;
-  // var navbarHeight = $('.nav-wrapper').outerHeight();
-  //
-  // $(window).scroll(function(event){
-  //     didScroll = true;
-  // });
-  //
-  // setInterval(function() {
-  //     if (didScroll) {
-  //         hasScrolled();
-  //         didScroll = false;
-  //     }
-  // }, 250);
-  //
-  // function hasScrolled() {
-  //     var distanceScrolled = $(this).scrollTop();
-  //
-  //     // Make sure they scroll more than delta (throttle scroll)
-  //     if(Math.abs(lastScrollTop - distanceScrolled) <= delta)
-  //         return;
-  //
-  //
-  //
-  //     // If they scrolled down and are past the navbar, add class .nav-up.
-  //     // This is necessary so you never see what is "behind" the navbar.
-  //     if (distanceScrolled > lastScrollTop && distanceScrolled > 30){
-  //         // Scroll Down
-  //         $('.nav-wrapper').removeClass('nav-down').addClass('nav-up');
-  //     } else {
-  //         // Scroll Up
-  //         if(distanceScrolled + $(window).height() < $(document).height()) {
-  //             $('.nav-wrapper').removeClass('nav-up').addClass('nav-down');
-  //         }
-  //     }
-  //     if (distanceScrolled > $(window).height() || distanceScrolled > 30 && $(window).width() <= 768) {
-  //       $('.nav-wrapper').addClass('scrolled-bg');
-  //     } else {
-  //       $('.nav-wrapper').removeClass('scrolled-bg');
-  //     }
-  //
-  //     lastScrollTop = distanceScrolled;
-  // }
-  //
-  // hasScrolled();
+
+  // Hide Header on on scroll down
+  if ($('.nav-wrapper').length > 0) {
+    let didScroll;
+    var lastScrollTop = 0;
+    var delta = 5;
+    var navbarHeight = $('.nav-wrapper').outerHeight();
+    var promoBarHeight = $('.promo-bar').outerHeight();
+    $('.nav-wrapper-placeholder').height(navbarHeight);
+    var navBarOffsetTop = $('.nav-wrapper').offset().top;
+    var triggerPoint = navbarHeight + 10;
+
+    $(window).on('resize', function () {
+      navbarHeight = $('.nav-wrapper').innerHeight();
+      $('.nav-wrapper-placeholder').height(navbarHeight);
+    });
+
+    $(window).scroll(function(event) {
+      didScroll = true;
+    });
+
+    setInterval(function() {
+      if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+      }
+    }, 250);
+
+    hasScrolled();
+
+    function hasScrolled() {
+      var distanceScrolled = $(window).scrollTop();
+      var isScrollingUp = distanceScrolled + $(window).height() < $(document).height();
+      var isScrollingDown = distanceScrolled > lastScrollTop && distanceScrolled > navbarHeight;
+
+      if (distanceScrolled < triggerPoint) {
+        $('.nav-wrapper').removeClass('scrolled scroll-up');
+        // if (distanceScrolled > navBarOffsetTop) {
+        //   $('.nav-wrapper').css('top', - (distanceScrolled - promoBarHeight) + 'px');
+        // }
+      } else {
+        $('.nav-wrapper').addClass('scrolled');
+        // Make sure they scroll more than delta
+        if (Math.abs(lastScrollTop - distanceScrolled) <= delta) return;
+
+        // If they scrolled down and are past the navbar, add class .scroll-down.
+        // This is necessary so you never see what is "behind" the navbar.
+
+        if (isScrollingDown) {
+          // Scroll Down
+          $('.nav-wrapper')
+            .removeClass('scroll-up')
+            .addClass('scroll-down');
+        } else if (isScrollingUp) {
+          // Scroll Up
+          $('.nav-wrapper')
+            .removeClass('scroll-down')
+            .addClass('scroll-up');
+        }
+
+        lastScrollTop = distanceScrolled;
+      }
+    }
+  }
+
+
 
   /* ---------------------------------------------
     SWIPER
@@ -161,6 +174,157 @@ $(document).ready(function() {
   //     disableOnInteraction: true
   //   }
   // });
+
+  /* ---------------------------------------------
+    PROMO SWIPER
+  ------------------------------------------------ */
+  if ($('.swiper-container-promo').length > 0) {
+    var promoSwiper = new Swiper('.swiper-container-promo', {
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: 'true'
+      },
+      speed: 400,
+      allowTouchMove: true,
+      loop: true,
+      effect: 'fade',
+      fadeEffect: {
+        crossFade: true
+      },
+      autoplay: {
+        delay: 8000,
+        disableOnInteraction: true,
+      },
+      navigation: {
+        nextEl: '.swiper-next',
+        prevEl: '.swiper-prev',
+      },
+    });
+  }
+
+
+  /* ---------------------------------------------
+    SPLIT TEXT / LIST ANIMATIONS
+  ------------------------------------------------ */
+  var controller = new ScrollMagic.Controller();
+  var tl = gsap.timeline();
+  document.fonts.ready.then(function () {
+    var mySplitText = new SplitText('[data-break="lines"]', {type:"lines", linesClass: 'split-line-++'});
+    new SplitText('[data-break="lines-masked"]', {type:"lines", linesClass: 'split-line-child'});
+    new SplitText('[data-break="lines-masked"]', {type:"lines", linesClass: 'split-line-++'});
+    new SplitText('[data-break="lines-masked-desc"] p', {type:"lines", linesClass: 'split-line-child'});
+    new SplitText('[data-break="lines-masked-desc"] p', {type:"lines", linesClass: 'split-line-++'});
+
+    $('[data-break="lines-masked"]').addClass('loaded');
+    $('[data-break="lines-masked-desc"]').addClass('loaded');
+
+    // resize function
+    $(window).resize(function() {
+      tl.progress(1);
+      mySplitText.revert();
+    });
+
+    $('.list-animation-wrapper').each(function() {
+      var scrollOffset = $(this).data('offset') || 0;
+      var $thisWrapper = $(this);
+      new ScrollMagic.Scene({
+        triggerElement: this,
+        offset: scrollOffset,
+        triggerHook: .85
+      })
+        .setClassToggle(this, "scrolled") // add class toggle
+        .reverse(false)
+
+        .addTo(controller)
+        .on("enter", function (e) {
+          if ($thisWrapper.find('.animate-item').length > 0) {
+            var $listItems = $thisWrapper.find('.animate-item');
+          } else {
+            var $listItems = $thisWrapper.find('li');
+          }
+
+
+          $listItems.each(function(i) {
+            var delay = i * 100;
+            var self = this;
+            setTimeout(function() {
+              $(self).addClass('fade-in');
+            }, delay);
+          });
+        });
+    });
+
+  // END OF FONT LOADING
+  });
+
+
+    /* ---------------------------------------------
+      PARALLAX & SCROLLING ZOOM EFFECTS
+    ------------------------------------------------ */
+    document.fonts.ready.then(function () {
+      $('[data-anim="scroll"]').each(function() {
+        var scrollWrapper = this;
+        var scrollOffset = $(this).data('offset') || 0;
+        new ScrollMagic.Scene(
+          {
+            triggerElement: this,
+            offset: scrollOffset,
+            triggerHook: .85
+          })
+          .setClassToggle(this, "js-animate") // add class toggle
+          // .addIndicators('classAdd')
+          .reverse(false)
+          .addTo(controller);
+
+
+          // SCALE IMAGE UP OR DOWN
+          var $imageToScale = $(this).find('[data-anim="scale"]')
+          if ($imageToScale.length > 0) {
+            var scaleFrom = $imageToScale.data('scale-from') != undefined ? parseFloat($imageToScale.data('scale-from')) : 1;
+            var scaleTo = $imageToScale.data('scale-to') != undefined ? parseFloat($imageToScale.data('scale-to')) : 1.09;
+            var pointToTrigger = $imageToScale.data('trigger-hook') != undefined ? parseFloat($imageToScale.data('trigger-hook')) : 0.4;
+            var dataDuration = $(this).data('duration') != undefined ? $(this).data('duration') : '100%';
+
+            var timelineScale = new TimelineMax();
+            var imageToAnimate = $imageToScale;
+            timelineScale.fromTo(imageToAnimate, 1, {scale: scaleFrom}, {scale: scaleTo});
+
+            var scaleScene = new ScrollMagic.Scene({
+              triggerElement: scrollWrapper,
+              triggerHook: pointToTrigger,
+              duration: dataDuration
+            })
+            .setTween(timelineScale)
+            .addTo(controller);
+          }
+
+          if ($(window).innerWidth() > 768) {
+            // PARALLAX EFFECT
+            var $itemToTranslate = $(this).find('[data-anim="parallax"]');
+            if ($itemToTranslate.length > 0) {
+              $itemToTranslate.each(function() {
+                var timelineParallax = new TimelineMax();
+                var translateFrom = $(this).data('translate-from') != undefined ? parseFloat($(this).data('translate-from')) : 40;
+                var translateTo = $(this).data('translate-to') != undefined ? parseFloat($(this).data('translate-to')) : -40;
+                var dataDuration = $(this).data('duration') != undefined ? $(this).data('duration') : '100%';
+                timelineParallax.fromTo($(this), 1, {y: translateFrom}, {y: translateTo});
+
+                var scene = new ScrollMagic.Scene({
+                  triggerElement: scrollWrapper,
+                  triggerHook: 0.4,
+                  duration: dataDuration
+                })
+                .setTween(timelineParallax)
+                .addTo(controller);
+              });
+            }
+          }
+
+      });
+
+    // END OF FONT LOADING
+    });
+
 
 
   /* ---------------------------------------------
